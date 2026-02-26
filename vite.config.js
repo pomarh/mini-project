7b6d894eed16a99cdf3bf3253a1f3a7e2eb258f9
Henry Poma <poma.hny@gmail.com>
2026-02-26 15:15:40 -0400

    revicion9

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
-
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        proyectos: resolve(__dirname, 'proyectos.html'),
+      },
+    },
+  },
+});
