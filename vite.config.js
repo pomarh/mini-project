3eaa1d1f9078fcbb2b5052d421cdafb8a228faa7
Henry Poma <poma.hny@gmail.com>
2026-02-26 20:41:19 -0400

    revicion 22

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        
+        proyectos: resolve(__dirname, 'proyectos.html')
       },
     },
   },
